0ed08dcfeba5966de22f3158ff0c96e054572434
AlexGanechkin <terracat80@gmail.com>
2023-02-03 23:08:21 +0400

    Домашнее задание. Часть 1

--- part1/practice/bad_smell_long_parameter_list/main.py
+++ part1/practice/bad_smell_long_parameter_list/main.py
@@ -9,42 +9,22 @@
 # В этом примере есть сразу несколько запахов плохого кода. Исправьте их
 #   (длинный метод, длинный список параметров)
 
-
 class Unit:
-    def move(self, field, x_coord, y_coord, direction, is_fly, crawl, speed = 1):
+    dir_list = {'x_axis': {'UP': 1, 'DOWN': -1}, 'y_axis': {'LEFT': 1, 'RIGTH': -1}}
+    state = {'fly': 1.2, 'crawl': 0.5}
+    # ...
+    def __init__(self):
+        pass
 
-        if is_fly and crawl:
-            raise ValueError('Рожденный ползать летать не должен!')
+    def move(self, direction):
+        speed = self._get_speed()
+        x = self.x + Unit.dir_list['x_axis'].get(direction, 0) * speed
+        y = self.y + Unit.dir_list['y_axis'].get(direction, 0) * speed
 
-        if is_fly:
-            speed *= 1.2
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
-        if crawl:
-            speed *= 0.5
-            if direction == 'UP':
-                new_y = y_coord + speed
-                new_x = x_coord
-            elif direction == 'DOWN':
-                new_y = y_coord - speed
-                new_x = x_coord
-            elif direction == 'LEFT':
-                new_y = y_coord
-                new_x = x_coord - speed
-            elif direction == 'RIGTH':
-                new_y = y_coord
-                new_x = x_coord + speed
+        self.field.set_unit(y=y, x=x, unit=self)
 
-            field.set_unit(x=new_x, y=new_y, unit=self)
+    def _get_speed(self, unit_state=1):
+        if unit_state not in Unit.state:
+            raise ValueError('Эк тебя раскорячило')
+        return Unit.state[unit_state]
 
-#     ...
